9ff27f776a0cb4b69d3726b1481849aec25838a1
Geraldo Macias <geraldomacias@geraldos-mbp.lan>
2020-09-08 17:50:12 -0700

    test

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
       </Nav>
       <Form inline>
         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        <Button variant="outline-success">Search</Button>
+        <Button variant="outline-success">Search...</Button>
       </Form>
     </Navbar.Collapse>
     </Navbar>
